99ddc11b41d3a2d62fc7b4d71446465a90506f72
saintlukas <lukestevens@gmail.com>
2012-11-12 22:01:04 -0500

    fixed export module

--- index.js
+++ index.js
@@ -4,4 +4,4 @@
 // Copyright (c) 2012 Aaron Westendorf. See LICENSE for further details.
 //
 // @see README.md for usage information
-module.exports = require('./lib/client').Client;
+module.exports = require('./lib/client');
